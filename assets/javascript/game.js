b7c4fccda5104181c8fb43a374aa10362d1a13d6
marianapt <mariana_pt3@hotmail.com>
2017-08-17 22:22:06 -0500

    css and html added

---
+++ assets/javascript/game.js
@@ -0,0 +1,29 @@
+//need a random number displayed on screen
+Math.floor(Math.random());
+
+var pink=Math.floor(Math.random());
+var 
+
+
+
+//function that grabs $("ID").on(click(funtion(
+//GRAB VALUE OF crystal on click will grab value
+//add the value of 1
+//4 times
+
+
+
+
+)))
+
+//set function start game and check condition if sum
+//total is > sum of crystal then loose  == win
+
+
+//reset game function 
+
+// keep count of wins and looses
+
+
+
+
